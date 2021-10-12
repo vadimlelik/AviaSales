e7375e67648c41289306327b67d6fdef96d4623c
Vadim Cvirko <promiti90@gmail.com>
2021-10-12 18:35:14 +0300

    Получение данных по билетам

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,40 @@
-import locations from './store/location'
+import '../css/style.css';
+import './plugins';
+import locations from './store/locations';
+import formUI from './views/form';
+import currencyUI from './views/currency';
 
+document.addEventListener('DOMContentLoaded', e => {
+  const form = formUI.form;
 
-locations.init()
-  .then(res => {
-    console.log(locations);
-    console.log(locations.getCitiesByCoyntryCode('PE'));
-  })
+  // Events
+  initApp();
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    onFormSubmit();
+  });
+
+  // handlers
+  async function initApp() {
+    await locations.init();
+    formUI.setAutocompleteData(locations.shortCities);
+  }
+
+  async function onFormSubmit() {
+    const origin = locations.getCityCodeByKey(formUI.originValue);
+    const destination = locations.getCityCodeByKey(formUI.destinationValue);
+    const depart_date = formUI.departDateValue;
+    const return_date = formUI.returnDateValue;
+    const currency = currencyUI.currecyValue;
+
+    await locations.fetchTickets({
+      origin,
+      destination,
+      depart_date,
+      return_date,
+      currency,
+    });
+
+    console.log(locations.lastSearch);
+  }
+});
